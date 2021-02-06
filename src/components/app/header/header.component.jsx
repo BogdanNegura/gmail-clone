fb85dfdcb06f8a9c520f1b2e7b6c800c42e7f7bb
Bogdan Negura <id.bogdann@yahoo.com>
2021-02-06 21:26:57 +0000

    add header sides and structure

--- src/components/app/header/header.component.jsx
+++ src/components/app/header/header.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import "./header.style.css"
 import MenuIcon from '@material-ui/icons/Menu';
-import { IconButton } from '@material-ui/core';
+import { Avatar, Icon, IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 const Header = () => {
     return (
@@ -10,12 +14,21 @@ const Header = () => {
                 <IconButton>
                     <MenuIcon/>
                 </IconButton>
+                <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt=""/>
             </div>
             <div className="header__middle">
-
+                <SearchIcon/>
+                <input type="text" placeholder="Search mail"/>
+                <ArrowDropDownIcon />
             </div>
             <div className="header__right">
-
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                <Avatar/>
             </div>
         </div>
     )
