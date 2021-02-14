13f304e06d5571ded56af227194af68ab7cec9a9
Bogdan Negura <id.bogdann@yahoo.com>
2021-02-14 21:52:08 +0000

    style the send mail box

--- src/components/send-mail/send-mail.component.jsx
+++ src/components/send-mail/send-mail.component.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import "./send-mail.style.css"
 import CloseIcon from "@material-ui/icons/Close"
+import { Button } from '@material-ui/core'
 
 const SendMail = () => {
     return (
         <div className="sendMail">
             <div className="sendMail__header">
                 <h3>New Message</h3>
-                <CloseIcon className="closeIcon"/>
+                <CloseIcon className="sendMail__close"/>
             </div>
+            <form >
+                <input placeholder="To" type="text"/>
+                <input placeholder="Subject" type="text"/>
+                <input placeholder="Message..." type="text" className="sendMail__message"/>
+
+                <div className="sendMail__options">
+                    <Button className="sendMail__send">Send</Button>
+                </div>
+            </form>
         </div>
     )
 }
