df0380b6b9508cbbdacf43bff33926f201b35f1d
Bogdan Negura <id.bogdann@yahoo.com>
2021-02-15 22:07:42 +0000

    useform set the form errors for the submit component

--- src/components/send-mail/send-mail.component.jsx
+++ src/components/send-mail/send-mail.component.jsx
@@ -2,21 +2,52 @@ import React from 'react'
 import "./send-mail.style.css"
 import CloseIcon from "@material-ui/icons/Close"
 import { Button } from '@material-ui/core'
+import { useForm } from "react-hook-form"
 
-const SendMail = () => {
+
+function SendMail() {
+
+    const { register, handleSubmit, watch, errors } = useForm() 
+
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
     return (
         <div className="sendMail">
             <div className="sendMail__header">
                 <h3>New Message</h3>
                 <CloseIcon className="sendMail__close"/>
             </div>
-            <form >
-                <input placeholder="To" type="text"/>
-                <input placeholder="Subject" type="text"/>
-                <input placeholder="Message..." type="text" className="sendMail__message"/>
-
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input 
+                    name="to" 
+                    placeholder="To" 
+                    type="text"
+                    ref={register({ required: true })}
+                />
+                {errors.to && <p className="sendMail__error">To is Required!</p>}
+                <input 
+                    name="subject" 
+                    placeholder="Subject" 
+                    type="text" 
+                    ref={register({ required: true })}
+                />
+                {errors.to && <p className="sendMail__error">Subject is Required!</p>}
+                <input 
+                    name="message" 
+                    placeholder="Message..." 
+                    type="text" className="sendMail__message" 
+                    ref={register({ required: true })}
+                />
+                {errors.to && <p className="sendMail__error">Message is Required!</p>}
                 <div className="sendMail__options">
-                    <Button className="sendMail__send">Send</Button>
+                    <Button 
+                        className="sendMail__send" 
+                        variant="contained" 
+                        color="primary" 
+                        type="submit"
+                    >Send</Button>
                 </div>
             </form>
         </div>
